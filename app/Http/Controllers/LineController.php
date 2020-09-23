ba8b4bed06859610ac0daa8a384cd41a2a9efe9d
kentotakise <kentot.ru.777@gmail.com>
2020-09-23 16:02:08 +0900

    メイン処理の枠組みを記述

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -14,32 +14,51 @@ use Illuminate\Support\Facades\Log;
 
 class LineController extends Controller
 {
+
 	public function webhook (Request $request) {
-		$lineAccessToken = env('LINE_ACCESS_TOKEN', "");
-		$lineChannelSecret = env('LINE_CHANNEL_SECRET', "");
+		$lineAccessToken = env("LINE_ACCESS_TOKEN", "");
+		$lineChannelSecret = env("LINE_CHANNEL_SECRET", "");
 
 		// 署名のチェック
 		$signature = $request->headers->get(HTTPHeader::LINE_SIGNATURE);
 		if (!SignatureValidator::validateSignature($request->getContent(), $lineChannelSecret, $signature)) {
-			// TODO: 不正アクセス
+			Log::debug("不正なアクセスがありました");
 			return;
 		}
 
 		$httpClient = new CurlHTTPClient($lineAccessToken);
-		$lineBot = new LINEBot($httpClient, ['channelSecret' => $lineChannelSecret]);
+		$lineBot = new LINEBot($httpClient, ["channelSecret" => $lineChannelSecret]);
 
 		try {
 			// イベント取得
 			$events = $lineBot->parseEventRequest($request->getContent(), $signature);
+
 			foreach ($events as $event) {
 				$message = $event->getText();
 				$replyToken = $event->getReplyToken();
-				$textMessage = new TextMessageBuilder($message);
-				$lineBot->replyMessage($replyToken, $textMessage);
+
+				if ($message == "@開始") {
+					// ゲーム開始
+					// Game::start(); 的な?
+					// 別ファイルに処理を書く
+
+					$lineBot->replyText($replyToken, "ゲームを開始します");
+				} else if ($message == "@終了") {
+					// ゲーム終了
+					// Game::end(); 的な?
+					// 別ファイルに処理を書く
+
+					$lineBot->replyText($replyToken, "お疲れ様でした！\nゲームを終了します");
+				} else {
+					// とりあえずおうむ返し
+					$lineBot->replyText($replyToken, $message);
+				}
 			}
+
 		} catch (\Exception $e) {
-			// TODO: 例外
+			Log::debug($e->getMessage());
 			return;
 		}
 	}
+
 }
---
+++ app/Http/Services/Game.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Http\Services;
+
+use Illuminate\Support\Facades\Log;
+
+class Game
+{
+	public static function start() {
+		// ゲームの開始時の処理を書く
+	}
+
+	public static function end() {
+		// ゲームが終了時の処理を書く
+	}
+}
